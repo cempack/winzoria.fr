a634cef5a1c070a1e78bfc87a3885708a37e8691
Elliot Moreau <73651345+cempack@users.noreply.github.com>
2024-08-07 18:51:16 +0200

    feat(api): Add revalidate flag to best-voters route

--- src/app/api/best-voters/route.ts
+++ src/app/api/best-voters/route.ts
@@ -1,6 +1,7 @@
 import { getBestVoters } from "@/db/queries/select";
 import { NextResponse } from "next/server";
 
+export const revalidate = 0;
 export async function GET(req: Request) {
   try {
     const bestVoters = await getBestVoters();
